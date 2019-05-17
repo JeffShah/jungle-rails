8c4d66eb5d0a570a815d8e78b07829b681779246
JaffarShah <sjeffshah@gmail.com>
2019-05-17 17:01:29 +0000

    Order details for the customers

--- config/routes.rb
+++ config/routes.rb
@@ -19,13 +19,12 @@ Rails.application.routes.draw do
   end
 
   #GifVault::Application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
-    get '/logout' => 'sessions#destroy'
-
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
+   get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   #end
     # This route sends requests to our naked url to the *cool* action in the *gif* controller.
